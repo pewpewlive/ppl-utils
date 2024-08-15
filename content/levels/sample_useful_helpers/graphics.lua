8e0fc674d022d25f72ce54db1c0072d1d6ba9c28
Jean-François Geyelin <jfgeyelin@gmail.com>
2024-08-15 13:56:52 +0200

    Remove sample that promoted obsolete practices

--- content/levels/sample_useful_helpers/graphics.lua
+++
@@ -1,9 +0,0 @@
-local helper = require("/dynamic/helpers/mesh_helpers.lua")
-
-meshes = {{
-  vertexes = {{0,0}, {0,300}, {300,300}, {300,0}},
-  segments = {{0,1,2,3,0}},
-  colors = {0xff0000ff, 0xff0000ff, 0xff0000ff, 0xff0000ff}
-}}
-
-helper.add_vertical_cylinder_to_mesh(meshes[1], {150,150,-200}, 400, 400, 100, 0xff0000ff)
