ebad92a5fb08db3f118a8c03bc0341b40382ee3a
SKPG-Tech <42890752+SKPG-Tech@users.noreply.github.com>
2025-06-22 21:16:02 +0300

    Update animation sample level

--- levels/sample_animation/graphics.lua
+++ levels/sample_animation/graphics.lua
@@ -1,33 +1,44 @@
 meshes = {}
 
-for angle_offset = 0, math.pi * 2, math.pi * 2 / 120 do -- Our animation will have 120 frames (60 interpolated frames). We want the hexagons to make a full rotation each 60 frames (2 seconds).
+local inner_radius = 48 -- Inner hexagon
+local outer_radius = 96 -- Outer hexagon
+
+-- Our animation will have 60 frames. We want the hexagons to make a full rotation each 60 frames (2 seconds).
+-- This does not mean the animation is 60FPS yet!
+for frame = 0, 119 do
   -- Tables for our mesh vertexes, segments
   local computed_vertexes, computed_segments = {}, {}
 
-  local radius = 96       -- Outer hexagon
-  local small_radius = 48 -- Inner hexagon
+  local inner_segment = {} -- Inner hexagon
+  local outer_segment = {} -- Outer hexagon
 
-  local i = 0
-  for angle = 0, math.pi * 2, math.pi * 2 / 6 do                              -- We want to go from angle 0 to 2π, skipping by 2π / 6, making a hexagon
-    local y, x = math.sincos(angle - angle_offset)                            -- y and x positions on the unit circle
-    local y2, x2 = math.sincos(angle + angle_offset)                          -- y and x positions on the unit circle for inner hexagon
+  local angle_offset = math.tau * frame / 120
 
-    table.insert(computed_vertexes, { x * radius, y * radius })               -- Vertex for the outer hexagon
-    table.insert(computed_vertexes, { x2 * small_radius, y2 * small_radius }) -- Vertex for the inner hexagon
+  local vertex_index = 0
+  for i = 0, 5 do -- We want to go from angle 0 to 2π, skipping by 2π / 6, making a hexagon
+    local angle = math.tau * i / 6
 
-    table.insert(computed_segments, { i, i + 1 })                             -- Line joining the corresponding vertices of the inner and outer hexagons
-    table.insert(computed_segments, { i, i + 2 })                             -- Line joining the vertex of the outer hexagon to its next one
-    table.insert(computed_segments, { i + 1, i + 3 })                         -- Line joining the vertex of the inner hexagon to its next one
+    local y1, x1 = math.sincos(angle + angle_offset)                          -- y and x positions on the unit circle
+    table.insert(computed_vertexes, { x1 * inner_radius, y1 * inner_radius }) -- Vertex for the inner hexagon
 
-    i = i + 2
+    local y2, x2 = math.sincos(angle - angle_offset)
+    table.insert(computed_vertexes, { x2 * outer_radius, y2 * outer_radius }) -- Vertex for the outer hexagon
+
+    table.insert(inner_segment, vertex_index)                                 -- Adding the vertex to the segment forming the inner hexagon
+    table.insert(outer_segment, vertex_index + 1)                             -- Adding the vertex to the segment forming the outer hexagon
+    table.insert(computed_segments, { vertex_index, vertex_index + 1 })       -- Line joining the corresponding vertexes of the inner and outer hexagons
+
+    vertex_index = vertex_index + 2
   end
 
-  table.remove(computed_segments, #computed_segments) -- Removal of the last segment as there is no vertex at index i + 3 during the last iteration
-  table.remove(computed_segments, #computed_segments) -- No vertex at index i + 2 during the last iteration
-  table.remove(computed_segments, #computed_segments) -- A line is already present joining the vertices
+  table.insert(inner_segment, 0) -- The first vertex of the inner hexagon has the index of 0
+  table.insert(outer_segment, 1) -- The first vertex of the outer hexagon has the index of 1, as it was generated after that of the inner hexagon
+
+  table.insert(computed_segments, inner_segment)
+  table.insert(computed_segments, outer_segment)
 
   table.insert(meshes, {
     vertexes = computed_vertexes,
-    segments = computed_segments,
+    segments = computed_segments
   })
 end
--- levels/sample_animation/level.lua
+++ levels/sample_animation/level.lua
@@ -1,12 +1,12 @@
 local id = pewpew.new_customizable_entity(0fx, 0fx)
-local mesh_index = 0
+pewpew.customizable_entity_start_spawning(id, 0)
+
+local tick = 0
 pewpew.entity_set_update_callback(id, function()
-  -- We have 120 frames out of 121 total frames. The last frame is equal to the first one and is unused.
-  -- The index of the 120th frame is 119. (Although we are using lua, mesh and sound indexes start from 0 in PewPew Lib API.)
-  -- Loop when we have exceeded past the last frame.
-  if mesh_index > 119 then
-    mesh_index = 0
-  end
-  pewpew.customizable_entity_set_flipping_meshes(id, "/dynamic/graphics.lua", mesh_index, mesh_index + 1)
-  mesh_index = mesh_index + 2
+  -- We have 120 frames.
+  -- The index of the 120th frame is 119. (Although we are using Lua, mesh and sound indexes start from 0 in PewPew Live API.)
+  -- Loop when we have exceeded past the last frame by using the modulo operator % in Lua.
+  pewpew.customizable_entity_set_flipping_meshes(id, "/dynamic/graphics.lua", (tick * 2) % 120, (tick * 2 + 1) % 120)
+
+  tick = tick + 1
 end)
